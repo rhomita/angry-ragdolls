abf4c5dee33c1f71dc3d07c31b7ace2a6d8b96ff
Rhomita <roman.glom@gmail.com>
2020-08-02 23:18:48 +0200

    Added the game's title on the menu and some music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Singleton
+    public static AudioManager instance { get; private set; }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+}
